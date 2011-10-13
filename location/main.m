4eb934a2a41dd1f321ab7ea01b13d4e9e2ae19cd
Britt Crawford <britt.v.crawford@gmail.com>
2011-10-13 13:52:14 -0700

    Initial Commit

---
+++ location/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  location
+//
+//  Created by Britt Crawford on 10/13/11.
+//  Copyright 2011 Bookrenter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
