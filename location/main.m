aae8f779d4047f2f2a39929b2d47393af82101e7
Britt Crawford <britt.v.crawford@gmail.com>
2011-10-13 14:18:17 -0700

    Added location services and a basic latitude longitude print out.

--- location/main.m
+++ location/main.m
@@ -3,18 +3,24 @@
 //  location
 //
 //  Created by Britt Crawford on 10/13/11.
-//  Copyright 2011 Bookrenter. All rights reserved.
+//  Copyright 2011 Britt Crawford. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
 
 int main (int argc, const char * argv[])
 {
 
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-    // insert code here...
-    NSLog(@"Hello, World!");
+    
+    if([CLLocationManager locationServicesEnabled]) {
+        CLLocationManager * geo = [[CLLocationManager alloc] init];
+        CLLocation *location = geo.location;
+        NSLog([NSString localizedStringWithFormat:@"%f,%f", location.coordinate.latitude, location.coordinate.longitude]);
+    } else {
+        NSLog(@"Location services are not enabled. You must enabled them to use location.");
+    }
 
     [pool drain];
     return 0;
